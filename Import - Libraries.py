b69be9b0167a36291ce7e7db543702264c3836d4
jekpoudom <76266003+jekpoudom@users.noreply.github.com>
2021-11-23 00:01:13 -0500

    Add files via upload

---
+++ Import - Libraries.py
@@ -0,0 +1,5 @@
+import random 
+import numpy as np
+import math
+#first I import numerous libraries that would be necessary to bring the project to life
+#random library helps in randomization. Numpy relates to arrays and mathematical functions 
