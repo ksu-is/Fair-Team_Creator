4c5e5d6fd7a4b80d809e00004173f75cabb83675
jekpoudom <76266003+jekpoudom@users.noreply.github.com>
2021-11-24 11:11:26 -0500

    Add files via upload
    We updated the code to shuffles all the collected names so that a team can be generated.

---
+++ ListShuffle.py
@@ -0,0 +1,30 @@
+import random 
+import numpy as np
+import math
+#first I import numerous libraries that would be necessary to bring the project to life
+#random library helps in randomization. Numpy relates to arrays and mathematical functions 
+
+#collect info
+
+#collect total players
+player_count=int(input("Enter participant count? "))
+team_count=int(input("How many teams do you want "))
+
+player_counts=int(player_count)
+team_counts=int(team_count)
+
+#to determine the count of members in each team, the total players count is divided by teams
+ratio=int(player_count/team_count)
+
+print(ratio)
+print("For this event, you will have ", ratio," players in ", team_count, " teams")
+#list to store and hold players for later use 
+player_list=[]
+
+#collect players names 
+for i in range(0,(player_counts)):
+    name=input("Enter player""'s name: ")
+    player_list.append(name)
+
+#this line shuffles all the collected names so that a team can be generated.
+np.random.shuffle(player_list)
